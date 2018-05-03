cd64c7ef084a19640049db205be23d36005cc540
Jesse Chan <jc@lineageos.org>
2018-05-02 17:09:59 -0700

    universal8895: add vibrator HAL for SEC Haptic Engine
    Change-Id: I758b40deb4e0b8bfda1aa9a24c27207d3f3d7dbe

--- device-common.mk
+++ device-common.mk
@@ -227,6 +227,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-service.universal8895
+
 # Wifi
 PRODUCT_PACKAGES += \
     macloader \
