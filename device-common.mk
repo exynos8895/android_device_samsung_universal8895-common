eb5d378c53f033d322ffefb1b8b5deaaa25c5f95
Stricted <info@stricted.net>
2018-09-22 00:19:57 +0200

    universal8895: build DRM hal
    Change-Id: I78a0ba3932c6ad82d77fdd83c8f84a3b35531464

--- device-common.mk
+++ device-common.mk
@@ -34,6 +34,16 @@ PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl \
     android.hardware.configstore@1.0-service
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0 \
+    android.hardware.drm@1.1 \
+    libfwdlockengine \
+    libdrmclearkeyplugin \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey \
+    android.hardware.drm@1.0-impl
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
