60683d9f5c06b5dcd83a3a55a79efae42ffea8b5
Jan Altensen <info@stricted.net>
2019-08-23 02:15:06 +0200

    universal8895: build camera packages
    Change-Id: Id10fbe3e006fc6752edaa99f6b436a95becb0165

--- device-common.mk
+++ device-common.mk
@@ -31,6 +31,18 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 # A list of dpis to select prebuilt apk, in precedence order.
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@1.0 \
+    android.hardware.camera.device@3.2 \
+    android.hardware.camera.device@3.3 \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.provider@2.4 \
+    Snap
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl \
