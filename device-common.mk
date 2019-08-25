bc18ee10e2575c954d22f80f195dcffb2b101aac
Jan Altensen <info@stricted.net>
2019-08-25 18:19:13 +0200

    universal8895: build gatekeeper hidl hal
    Change-Id: Iaf9e473fd892582bc1598cf3092e1f3c4ae0bc5e

--- device-common.mk
+++ device-common.mk
@@ -62,6 +62,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     dtbhtoolExynos
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
