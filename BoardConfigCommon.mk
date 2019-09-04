9869bf9c8679243c1aa09df0693f045b53f3f9e0
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:50:41 +0430

    universal8895: Setup device soong namespace
    Change-Id: I2e9c0ab68a0fe86ba18e3955e2823b87d88e835a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -147,3 +147,6 @@ BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
 TARGET_LD_SHIM_LIBS += \
     /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
     /system/lib64/libexynoscamera.so|/vendor/lib64/libexynoscamera_shim.so
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
