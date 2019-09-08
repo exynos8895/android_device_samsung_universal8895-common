34bd4dfb5e505a8cae83dbf0caf935c78ce34fe7
dianlujitao <dianlujitao@lineageos.org>
2019-09-08 13:39:42 +0800

    universal8895: Remove custom kernel cross compile prefix
    Change-Id: Icc58f38fa2f66b833c73902d6733f84b77dcb575

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,7 +69,6 @@ BOARD_ROOT_EXTRA_FOLDERS += efs cpefs
 # Kernel
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Kernel config
 TARGET_KERNEL_SOURCE := kernel/samsung/universal8895
