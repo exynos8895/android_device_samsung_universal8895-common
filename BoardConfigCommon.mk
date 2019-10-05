0430d033ae6ac6e3c39db3e705423fc0d063cd9e
Michael Benedict <michaelbt@live.com>
2019-10-05 20:15:02 +1000

    universal8895: build apex image
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,6 +54,9 @@ TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 # Binder
 TARGET_USES_64_BIT_BINDER := true
 
+# Apex
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Extracted with libbootimg
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
