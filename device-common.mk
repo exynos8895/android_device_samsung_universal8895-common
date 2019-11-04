58812dc447d95a5fe9760046a92a18f71b503ebe
Jan Altensen <info@stricted.net>
2019-11-04 20:39:18 +0100

    universal8895: switch to passthrough camera provider
    Change-Id: I74174fb51b4b7da001a6a04fddcf931ce29c3f7a

--- device-common.mk
+++ device-common.mk
@@ -59,7 +59,6 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     android.hardware.camera.common@1.0 \
     android.hardware.camera.device@1.0 \
     android.hardware.camera.device@3.2 \
