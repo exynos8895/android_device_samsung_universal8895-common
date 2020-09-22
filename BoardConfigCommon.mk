a3e977de3a2f5c10dda66cf751a3d5d6ce9508bb
Jan Altensen <info@stricted.net>
2020-09-22 23:38:27 +0400

    universal8895: BoardConfig: ditch custom Exynos mtunes

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,7 +42,7 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := exynos-m1
+TARGET_CPU_VARIANT := generic
 
 # Secondary Architecture
 TARGET_2ND_ARCH := arm
